67c086a8b619c19488f360cea6d137f5ee49313f
Patrick Lauber <digi@treepy.com>
2010-11-10 16:06:57 +0100

    fix for when queryset is given to the formfield

--- smart_selects/form_fields.py
+++ smart_selects/form_fields.py
@@ -10,21 +10,24 @@ class ChainedModelChoiceField(ModelChoiceField):
             'widget': ChainedSelect(app_name, model_name, chain_field, model_field, show_all, auto_choose),
         }
         defaults.update(kwargs)
-        queryset = get_model(app_name, model_name).objects.all()
-        super(ChainedModelChoiceField, self).__init__(queryset=queryset, initial=initial, *args, **defaults)
-    
+        if not 'queryset' in kwargs:
+            queryset = get_model(app_name, model_name).objects.all()
+            super(ChainedModelChoiceField, self).__init__(queryset=queryset, initial=initial, *args, **defaults)
+        else:
+            super(ChainedModelChoiceField, self).__init__(initial=initial, *args, **defaults)
+
     def _get_choices(self):
         self.widget.queryset = self.queryset
         choices = super(ChainedModelChoiceField, self)._get_choices()
         return choices
     choices = property(_get_choices, ChoiceField._set_choices)
-    
-    
+
+
 class GroupedModelSelect(ModelChoiceField):
     def __init__(self, queryset, order_field, *args, **kwargs):
         self.order_field = order_field
         super(GroupedModelSelect, self).__init__(queryset, *args, **kwargs)
-        
+
     def _get_choices(self):
         # If self._choices is set, then somebody must have manually set
         # the property self.choices. In this case, just return self._choices.
@@ -37,7 +40,7 @@ class GroupedModelSelect(ModelChoiceField):
         # accessed) so that we can ensure the QuerySet has not been consumed. This
         # construct might look complicated but it allows for lazy evaluation of
         # the queryset.
-        final = [("",self.empty_label or "---------"),]
+        final = [("", self.empty_label or "---------"), ]
         group = None
         for item in self.queryset:
             if not group or group[0] != unicode(getattr(item, self.order_field)):
@@ -46,11 +49,11 @@ class GroupedModelSelect(ModelChoiceField):
                 group = [unicode(getattr(item, self.order_field)), []]
             group[1].append(self.make_choice(item))
         return final
-    
+
     def make_choice(self, obj):
-        return (obj.pk, "   "+self.label_from_instance(obj))
+        return (obj.pk, "   " + self.label_from_instance(obj))
 
     choices = property(_get_choices, ChoiceField._set_choices)
 
 
-    
+
