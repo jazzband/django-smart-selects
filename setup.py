60e5b617e36d174b9b866bed6c5fce067f3b6a8e
Patrick Lauber <digi@treepy.com>
2011-01-17 16:41:57 +0100

    tag 1.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="django-smart-selects",
-           version="1.0.0",
+           version="1.0.1",
            description="Django application to handle chained model fields.",
            author="Patrick Lauber",
            packages=find_packages(),
