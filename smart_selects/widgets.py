f0a3f54718a74503f5a06c2f868822f60ce9ebd8
Ionel Maries Cristian <ionel.mc@gmail.com>
2011-06-11 21:18:28 +0300

    Wrap dismissAddAnotherPopup so change handler is triggered after add popup is closed.

--- smart_selects/widgets.py
+++ smart_selects/widgets.py
@@ -119,6 +119,13 @@ class ChainedSelect(Select):
                     fill_field(val, start_value);
                 })
             })
+            var oldDismissAddAnotherPopup = dismissAddAnotherPopup;
+            dismissAddAnotherPopup = function(win, newId, newRepr) {
+                oldDismissAddAnotherPopup(win, newId, newRepr);
+                if (windowname_to_id(win.name) == "id_%(chainfield)s") {
+                    $("#id_%(chainfield)s").change();
+                }
+            }
         })(jQuery || django.jQuery);
         //]]>
         </script>
