f00a2e0b200117b9024f6b66a46fa4f7dc463640
Pablo Martín <goinnn@gmail.com>
2013-06-13 11:15:38 +0200

    Ready to new release

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(name="django-smart-selects",
-           version="1.0.6",
-           description="Django application to handle chained model fields.",
-           author="Patrick Lauber",
-           packages=find_packages(),
-           include_package_data=True,
-)
-
+      version="1.0.7",
+      description="Django application to handle chained model fields.",
+      author="Patrick Lauber",
+      packages=find_packages(),
+      include_package_data=True,
+      )
