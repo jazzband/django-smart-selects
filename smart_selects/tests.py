38ab1f7a40f5d69bbc693e81bcc3e55f6906952c
Joel Crocker <joelcrocker@gmail.com>
2014-09-22 12:17:40 -0400

    Adds test script and basic tests of deconstruct() methods.

---
+++ smart_selects/tests.py
@@ -0,0 +1,55 @@
+import unittest
+
+from db_fields import ChainedForeignKey, GroupedForeignKey
+
+
+class AssertReconstructibleMixin(object):
+    def _assert_reconstructible(self, *field_args, **field_kwargs):
+        field_instance = self.field_class(*field_args, **field_kwargs)
+        name, path, args, kwargs = field_instance.deconstruct()
+        new_instance = self.field_class(*args, **kwargs)
+
+        for attr_name in self.deconstruct_attrs:
+            self.assertEqual(
+                getattr(field_instance, attr_name),
+                getattr(new_instance, attr_name)
+            )
+
+
+class ChainedForeignKeyTests(AssertReconstructibleMixin, unittest.TestCase):
+    def setUp(self):
+        self.field_class = ChainedForeignKey
+        self.deconstruct_attrs = [
+            'chain_field', 'model_field', 'show_all', 'auto_choose',
+            'view_name',
+        ]
+
+    def test_deconstruct_basic(self):
+        self._assert_reconstructible(
+            'myapp.MyModel',
+            chained_field='a_chained_field',
+            chained_model_field='the_chained_model_field',
+            show_all=False, auto_choose=True
+        )
+
+    def test_deconstruct_mostly_default(self):
+        self._assert_reconstructible(
+            'myapp.MyModel'
+        )
+
+    def test_deconstruct_non_default(self):
+        self._assert_reconstructible(
+            'myapp.MyModel',
+            chained_field='a_chained_field',
+            chained_model_field='the_chained_model_field',
+            show_all=True, auto_choose=True
+        )
+
+
+class GroupedForeignKeyTests(AssertReconstructibleMixin, unittest.TestCase):
+    def setUp(self):
+        self.field_class = GroupedForeignKey
+        self.deconstruct_attrs = ['group_field']
+
+    def test_deconstruct_basic(self):
+        self._assert_reconstructible('myapp.MyModel', 'the_group_field')
