9cf3562c6f96eff9182015d0acb17626d439a7ea
Tom McCarthy <tom.mccarthy@nyxinteractive.com>
2015-03-03 14:19:29 +0100

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="django-smart-selects",
-      version="1.0.9.3",
+      version="1.1.0",
       description="Django application to handle chained model fields.",
       author="Patrick Lauber",
       packages=find_packages(),
