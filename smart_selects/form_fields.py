d65f80980710771d4dce413b42d3c062c1ec55b4
Petr Dlouhý <petr.dlouhy@email.cz>
2015-08-06 00:51:44 +0200

    fix for Django 1.9: import get_model

--- smart_selects/form_fields.py
+++ smart_selects/form_fields.py
@@ -1,4 +1,8 @@
-from django.db.models import get_model
+try:
+    from django.apps import apps
+    get_model = apps.get_model
+except ImportError:
+    from django.db.models.loading import get_model
 from django.forms.models import ModelChoiceField
 from django.forms import ChoiceField
 
