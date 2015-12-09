766b6bbf56c8f8e120da61b243bf48f8ed9f32fd
jimmy_home <jimmy@motherapp.com>
2015-12-09 21:34:29 +0800

    [refactor] move the javascript of chainedforeignkey to js file

--- smart_selects/widgets.py
+++ smart_selects/widgets.py
@@ -55,8 +55,11 @@ class ChainedSelect(Select):
             js = [static('admin/js/%s' % url) for url in js]
         elif JQUERY_URL:
             js = [JQUERY_URL]
+        js = js + [static('smart-selects/admin/js/chainedfk.js')]
 
     def render(self, name, value, attrs=None, choices=()):
+        # raise Exception
+
         if len(name.split('-')) > 1:  # formset
             chain_field = '-'.join(name.split('-')[:-1] + [self.chain_field])
         else:
@@ -90,95 +93,28 @@ class ChainedSelect(Select):
             empty_label = iterator.__next__()[1]
         else:
             empty_label = iterator.next()[1]  # Hacky way to getting the correct empty_label from the field instead of a hardcoded '--------'
+
         js = """
         <script type="text/javascript">
-        //<![CDATA[
         (function($) {
-            function fireEvent(element,event){
-                if (document.createEventObject){
-                // dispatch for IE
-                var evt = document.createEventObject();
-                return element.fireEvent('on'+event,evt)
-                }
-                else{
-                // dispatch for firefox + others
-                var evt = document.createEvent("HTMLEvents");
-                evt.initEvent(event, true, true ); // event type,bubbling,cancelable
-                return !element.dispatchEvent(evt);
-                }
-            }
+            var chainfield = "#id_%(chainfield)s";
+            var url = "%(url)s";
+            var id = "#%(id)s";
+            var value = %(value)s;
+            var auto_choose = %(auto_choose)s;
+            var empty_label = "%(empty_label)s";
 
-            function dismissRelatedLookupPopup(win, chosenId) {
-                var name = windowname_to_id(win.name);
-                var elem = document.getElementById(name);
-                if (elem.className.indexOf('vManyToManyRawIdAdminField') != -1 && elem.value) {
-                    elem.value += ',' + chosenId;
-                } else {
-                    elem.value = chosenId;
-                }
-                fireEvent(elem, 'change');
-                win.close();
-            }
-
-            $(document).ready(function(){
-                function fill_field(val, init_value){
-                    if (!val || val==''){
-                        options = '<option value="">%(empty_label)s<'+'/option>';
-                        $("#%(id)s").html(options);
-                        $('#%(id)s option:first').attr('selected', 'selected');
-                        $("#%(id)s").trigger('change');
-                        return;
-                    }
-                    $.getJSON("%(url)s/"+val+"/", function(j){
-                        var options = '<option value="">%(empty_label)s<'+'/option>';
-                        for (var i = 0; i < j.length; i++) {
-                            options += '<option value="' + j[i].value + '">' + j[i].display + '<'+'/option>';
-                        }
-                        var width = $("#%(id)s").outerWidth();
-                        $("#%(id)s").html(options);
-                        if (navigator.appVersion.indexOf("MSIE") != -1)
-                            $("#%(id)s").width(width + 'px');
-                        $('#%(id)s option:first').attr('selected', 'selected');
-                        var auto_choose = %(auto_choose)s;
-                        if(init_value){
-                            $('#%(id)s option[value="'+ init_value +'"]').attr('selected', 'selected');
-                        }
-                        if(auto_choose && j.length == 1){
-                            $('#%(id)s option[value="'+ j[0].value +'"]').attr('selected', 'selected');
-                        }
-                        $("#%(id)s").trigger('change');
-                    })
-                }
-
-                if(!$("#id_%(chainfield)s").hasClass("chained")){
-                    var val = $("#id_%(chainfield)s").val();
-                    fill_field(val, "%(value)s");
-                }
-
-                $("#id_%(chainfield)s").change(function(){
-                    var start_value = $("#%(id)s").val();
-                    var val = $(this).val();
-                    fill_field(val, start_value);
-                })
-            })
-            if (typeof(dismissAddAnotherPopup) !== 'undefined') {
-                var oldDismissAddAnotherPopup = dismissAddAnotherPopup;
-                dismissAddAnotherPopup = function(win, newId, newRepr) {
-                    oldDismissAddAnotherPopup(win, newId, newRepr);
-                    if (windowname_to_id(win.name) == "id_%(chainfield)s") {
-                        $("#id_%(chainfield)s").change();
-                    }
-                }
-            }
+            $(document).ready(function() {
+                chainedfk.init(chainfield, url, id, value, empty_label, auto_choose);
+            });
         })(jQuery || django.jQuery);
-        //]]>
         </script>
 
         """
         js = js % {"chainfield": chain_field,
                    "url": url,
                    "id": attrs['id'],
-                   'value': value,
+                   'value': 'undefined' if value is None else value,
                    'auto_choose': auto_choose,
                    'empty_label': escape(empty_label)}
         final_choices = []
@@ -232,6 +168,7 @@ class ChainedSelect(Select):
             filtered = []
 
         return filtered
+
 
 class ChainedSelectMultiple(SelectMultiple):
     def __init__(self, app_name, model_name, chain_field, model_field,
