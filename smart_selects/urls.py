ea651df94b9b2dbc9fe9f395557ad2215b29b371
Petr Dlouhý <petr.dlouhy@email.cz>
2016-01-16 19:06:14 +0100

    Django 1.10 compatibility fix - urls.py

--- smart_selects/urls.py
+++ smart_selects/urls.py
@@ -1,14 +1,14 @@
 try:
-    from django.conf.urls.defaults import patterns, url
+    from django.conf.urls.defaults import url
 except ImportError:
-    from django.conf.urls import patterns, url
+    from django.conf.urls import url
+from .views import filterchain, filterchain_all
 
-urlpatterns = patterns(
-    'smart_selects.views',
+urlpatterns = [
     url(r'^all/(?P<app>[\w\-]+)/(?P<model>[\w\-]+)/(?P<field>[\w\-]+)/(?P<foreign_key_app_name>[\w\-]+)/(?P<foreign_key_model_name>[\w\-]+)/(?P<foreign_key_field_name>[\w\-]+)/(?P<value>[\w\-]+)/$',
-        'filterchain_all', name='chained_filter_all'),
+        filterchain_all, name='chained_filter_all'),
     url(r'^filter/(?P<app>[\w\-]+)/(?P<model>[\w\-]+)/(?P<field>[\w\-]+)/(?P<foreign_key_app_name>[\w\-]+)/(?P<foreign_key_model_name>[\w\-]+)/(?P<foreign_key_field_name>[\w\-]+)/(?P<value>[\w\-]+)/$',
-        'filterchain', name='chained_filter'),
+        filterchain, name='chained_filter'),
     url(r'^filter/(?P<app>[\w\-]+)/(?P<model>[\w\-]+)/(?P<manager>[\w\-]+)/(?P<field>[\w\-]+)/(?P<foreign_key_app_name>[\w\-]+)/(?P<foreign_key_model_name>[\w\-]+)/(?P<foreign_key_field_name>[\w\-]+)/(?P<value>[\w\-]+)/$',
-        'filterchain', name='chained_filter'),
-)
+        filterchain, name='chained_filter'),
+]
