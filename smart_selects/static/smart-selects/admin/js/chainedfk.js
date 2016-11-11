d42bfe9634831dc6f1bad4a6c5591f6b548bc82f
Dmitry <d.akinin@gmail.com>
2016-11-11 16:56:47 +0300

    Change .attr('selected', 'selected') to .prop("selected", true)
    Safari doesn't change select's option after .attr('selected', 'selected'). Need use .prop("selected", true) instead.
    Fix https://github.com/digi604/django-smart-selects/issues/162

--- smart_selects/static/smart-selects/admin/js/chainedfk.js
+++ smart_selects/static/smart-selects/admin/js/chainedfk.js
@@ -31,7 +31,7 @@
                 if (!val || val === ''){
                     var options = '<option value="">' + empty_label +'</option>';
                     $(elem_id).html(options);
-                    $(elem_id + ' option:first').attr('selected', 'selected');
+                    $(elem_id + ' option:first').prop('selected', true);
                     $(elem_id).trigger('change');
                     return;
                 }
@@ -45,12 +45,12 @@
                     if (navigator.appVersion.indexOf("MSIE") != -1)
                         $(elem_id).width(width + 'px');
                     if(init_value){
-                        $(elem_id + ' option[value="'+ init_value +'"]').attr('selected', 'selected');
+                        $(elem_id + ' option[value="'+ init_value +'"]').prop('selected', true);
                     } else {
-                        $(elem_id + ' option:first').attr('selected', 'selected');
+                        $(elem_id + ' option:first').prop('selected', true);
                     }
                     if(auto_choose && j.length == 1){
-                        $(elem_id + ' option[value="'+ j[0].value +'"]').attr('selected', 'selected');
+                        $(elem_id + ' option[value="'+ j[0].value +'"]').prop('selected', true);
                     }
                     $(elem_id).trigger('change');
                 });
