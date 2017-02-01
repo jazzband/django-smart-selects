615b92a1eaa2fc097089def8e5f9b77640866992
Drew Hubl <drew.hubl@gmail.com>
2017-02-01 11:47:45 -0700

    Bump version for new PyPI release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="django-smart-selects",
-      version="1.2.7",
+      version="1.2.8",
       description="Django application to handle chained model fields.",
       author="Patrick Lauber",
       author_email="digi@treepy.com",
