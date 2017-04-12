bb70fc6ca899cbeb3823c2fd4e7e752a0454e0c7
Serhiy Romanov <serhiy.romanov@gmail.com>
2017-04-12 16:02:34 +0300

    Fix StopIteration Exception when use it with django-bootstrap3 and have field without choices

--- smart_selects/widgets.py
+++ smart_selects/widgets.py
@@ -110,12 +110,15 @@ class ChainedSelect(JqueryMediaMixin, Select):
             auto_choose = 'true'
         else:
             auto_choose = 'false'
-        iterator = iter(self.choices)
-        if hasattr(iterator, '__next__'):
-            empty_label = iterator.__next__()[1]
+        if choices:
+	        iterator = iter(self.choices)
+	        if hasattr(iterator, '__next__'):
+	            empty_label = iterator.__next__()[1]
+	        else:
+	            # Hacky way to getting the correct empty_label from the field instead of a hardcoded '--------'
+	            empty_label = iterator.next()[1]
         else:
-            # Hacky way to getting the correct empty_label from the field instead of a hardcoded '--------'
-            empty_label = iterator.next()[1]
+	    	empty_label = "--------"
 
         js = """
         <script type="text/javascript">
