85b6a331ad3f03a7b7749f7c65bb8142bd75458c
Victor Borràs <victor@dekalabs.com>
2017-04-19 17:46:30 +0200

    change undefined and empty strings to null in tests

--- test_app/tests.py
+++ test_app/tests.py
@@ -24,7 +24,7 @@ class ViewTests(TestCase):
         response = self.client.get(reverse('admin:test_app_location_add'), follow=True)
         self.assertContains(response, 'Europe')
         self.assertContains(response, 'America')
-        self.assertContains(response, 'data-value="undefined"')
+        self.assertContains(response, 'data-value="null"')
 
     def test_location_add_post(self):
         post_data = {
@@ -47,7 +47,7 @@ class ViewTests(TestCase):
         }
         response = self.client.post(reverse('admin:test_app_location_add'), post_data)
         self.assertContains(response, 'This field is required.')
-        self.assertContains(response, 'data-value="undefined"')
+        self.assertContains(response, 'data-value="null"')
 
     def test_location_change_get(self):
         response = self.client.get(reverse('admin:test_app_location_change', args=(1,)))
@@ -86,7 +86,7 @@ class ViewTests(TestCase):
     def test_book_add_get(self):
         response = self.client.get(reverse('admin:test_app_book_add'))
         self.assertContains(response, 'Publication 1')
-        self.assertContains(response, 'data-value="&quot;&quot;"')
+        self.assertContains(response, 'data-value="null"')
 
     def test_book_add_post(self):
         post_data = {
