3ee58675dd1324d8553251df07addf5c8bdb656e
Ernesto Rodriguez Ortiz <ernesto.rodriguezortiz@savoirfairelinux.com>
2017-07-05 15:55:35 -0400

    Loop over all .chained when adding a new formset
    When adding a new formset with multiple chainedM2M any work because we only iterate
    in all $('.chained-fk') and not in $('.chained')

--- smart_selects/static/smart-selects/admin/js/bindfields.js
+++ smart_selects/static/smart-selects/admin/js/bindfields.js
@@ -26,31 +26,44 @@
             initItem(item);
         })
     });
-    // Fired every time a new inline formset is created
+
+    function initFormset (chained) {
+        var chainfield = $(chained).attr("data-chainfield");
+        if (chainfield.indexOf("__prefix__") > -1) {
+            /*
+             If we have several inlines with the same name, they will get an index, so we need to ignore that and get
+             the last numeric value in the id
+             */
+            var chainedId = $(chained).attr("id");
+            var re = /\d+/g;
+            var prefix;
+            var match;
+            do {
+                match = re.exec(chainedId);
+                if (match) {
+                    prefix = match[0];
+                }
+            } while (match);
+
+            chainfield = chainfield.replace("__prefix__", prefix);
+            $(chained).attr("data-chainfield", chainfield)
+        }
+        initItem(chained);
+    }
+
     django.jQuery(document).on('formset:added', function (event, $row, formsetName) {
+        // Fired every time a new inline formset is created
+
+        // For the ForeingKey
         var chainedFK = $row.find(".chained-fk");
         $.each(chainedFK, function (index, chained) {
-            var chainfield = $(chained).attr("data-chainfield");
-            if (chainfield.indexOf("__prefix__") > -1) {
-                /*
-                 If we have several inlines with the same name, they will get an index, so we need to ignore that and get
-                 the last numeric value in the id
-                 */
-                var chainedId = $(chained).attr("id");
-                var re = /\d+/g;
-                var prefix;
-                var match;
-                do {
-                    match = re.exec(chainedId);
-                    if (match) {
-                        prefix = match[0];
-                    }
-                } while (match);
+            initFormset(chained);
+        });
 
-                chainfield = chainfield.replace("__prefix__", prefix);
-                $(chained).attr("data-chainfield", chainfield)
-            }
-            initItem(chained);
+        // For the ManyToMany
+        var chainedM2M = $row.find(".chained");
+        $.each(chainedM2M, function (index, chained) {
+            initFormset(chained);
         });
     });
 })(jQuery || django.jQuery);
