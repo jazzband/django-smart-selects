fb065c75e180b0fed30a8751f47d57f8bb57da1d
Nuwan Goonasekera <nuwan.ag@gmail.com>
2017-12-18 01:21:20 +0530

    Removed unused import and fixed build matrix

--- test_app/settings.py
+++ test_app/settings.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import django
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
