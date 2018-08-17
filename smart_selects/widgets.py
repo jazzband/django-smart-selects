00b533c9aa057ee008c363cea3c75633e169b4b3
Gianni Moschini <gianni.moschini+github@gmail.com>
2018-08-18 00:54:14 +0100

    force to load select2 js first

--- smart_selects/widgets.py
+++ smart_selects/widgets.py
@@ -204,13 +204,16 @@ class ChainedSelectMultiple(JqueryMediaMixin, SelectMultiple):
     def media(self):
         """Media defined as a dynamic property instead of an inner class."""
         media = super(ChainedSelectMultiple, self).media
-        js = ['smart-selects/admin/js/chainedm2m.js',
-              'smart-selects/admin/js/bindfields.js']
+        js = []
         if self.horizontal:
             # For horizontal mode add django filter horizontal javascript code
             js.extend(["admin/js/core.js",
                        "admin/js/SelectBox.js",
                        "admin/js/SelectFilter2.js"])
+        js.extend([
+            'smart-selects/admin/js/chainedm2m.js',
+            'smart-selects/admin/js/bindfields.js'
+        ])
         media += Media(js=js)
         return media
 
