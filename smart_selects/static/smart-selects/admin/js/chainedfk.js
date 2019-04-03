343e2d2eef1f3a94afc00a124036437e6696de0f
James Hollingsworth <james.hollingsworth@quintia.com>
2019-04-03 08:33:12 +0100

    Should now be compatible with Django 2.x autocomplete fields

--- smart_selects/static/smart-selects/admin/js/chainedfk.js
+++ smart_selects/static/smart-selects/admin/js/chainedfk.js
@@ -2,6 +2,8 @@
     // We're declaring global variables, so we cannot use strict mode
     // "use strict";
 
+    var prevID = -1;
+
     chainedfk = function() {
         return {
             fireEvent: function (element, event) {
@@ -87,8 +89,21 @@
                     val = $(chainfield).val();
                     fill_field(val, init_value, id, url, empty_label, auto_choose);
                 }
-                $(document).on('change',$(chainfield), function(){
-                //$(chainfield).change(function () {
+                $("body").on('DOMSubtreeModified', "#select2-"+chainfield.slice(1)+"-container", function () {
+                    var prefix, start_value, localID = id;
+                    var this_val = prevID;
+                    var this_title = $(this).attr("title");
+                    if (typeof this_title != "undefined") {
+                       this_val = $(chainfield+' option').filter(function () { return $(this).html() == this_title; }).val();
+                    }
+                    if (this_val!=prevID) {
+                       prevID = this_val;
+                       fill_field(this_val, start_value, localID, url, empty_label, auto_choose);
+                    }
+                 });
+
+                $(chainfield).on(eventType, function () {
+
                     // Handle the case of inlines, where the ID will depend on which list item we are dealing with
                     var prefix, start_value, this_val, localID = id;
                     if (localID.indexOf("__prefix__") > -1) {
@@ -97,7 +112,12 @@
                     }
 
                     start_value = $(localID).val();
-                    this_val = $(this).val();
+                    if ($(this).hasClass("admin-autocomplete")) {
+                        this_val = $(this).children(":last-child").attr("value");
+                    } else {
+                        this_val = $(this).val();
+                    }
+                    //this_val = $('#id_hotel option:last-child').attr("value");
                     fill_field(this_val, start_value, localID, url, empty_label, auto_choose);
                 });
                 if (typeof(dismissAddAnotherPopup) !== 'undefined') {
