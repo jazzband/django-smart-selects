7fabb0a492aab8eac6b13f019c308f045838b8dd
James Hollingsworth <james.hollingsworth@quintia.com>
2019-04-06 16:07:58 +0100

    Commented out invalid code

--- smart_selects/static/smart-selects/admin/js/chainedfk.js
+++ smart_selects/static/smart-selects/admin/js/chainedfk.js
@@ -102,24 +102,24 @@
                     }
                  });
 
-                $(chainfield).on(eventType, function () {
+                #$(chainfield).on(eventType, function () {
 
-                    // Handle the case of inlines, where the ID will depend on which list item we are dealing with
-                    var prefix, start_value, this_val, localID = id;
-                    if (localID.indexOf("__prefix__") > -1) {
-                        prefix = $(this).attr("id").match(/\d+/)[0];
-                        localID = localID.replace("__prefix__", prefix);
-                    }
+                #    // Handle the case of inlines, where the ID will depend on which list item we are dealing with
+                #    var prefix, start_value, this_val, localID = id;
+                #    if (localID.indexOf("__prefix__") > -1) {
+                #        prefix = $(this).attr("id").match(/\d+/)[0];
+                #        localID = localID.replace("__prefix__", prefix);
+                #    }
 
-                    start_value = $(localID).val();
-                    if ($(this).hasClass("admin-autocomplete")) {
-                        this_val = $(this).children(":last-child").attr("value");
-                    } else {
-                        this_val = $(this).val();
-                    }
-                    //this_val = $('#id_hotel option:last-child').attr("value");
-                    fill_field(this_val, start_value, localID, url, empty_label, auto_choose);
-                });
+                #    start_value = $(localID).val();
+                #    if ($(this).hasClass("admin-autocomplete")) {
+                #        this_val = $(this).children(":last-child").attr("value");
+                #    } else {
+                #        this_val = $(this).val();
+                #    }
+                #    //this_val = $('#id_hotel option:last-child').attr("value");
+                #    fill_field(this_val, start_value, localID, url, empty_label, auto_choose);
+                #});
                 if (typeof(dismissAddAnotherPopup) !== 'undefined') {
                     var oldDismissAddAnotherPopup = dismissAddAnotherPopup;
                     dismissAddAnotherPopup = function (win, newId, newRepr) {
