60022d061be96eacbad21c1f0bafc562c88edb16
d9pouces <github@19pouces.net>
2020-08-19 21:06:58 +0200

    #308 add Django 3.1 as supported

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name="django-smart-selects",
       url="https://github.com/jazzband/django-smart-selects",
       packages=find_packages(),
       include_package_data=True,
-      install_requires=["django>=1.8, <3.1", "six"],
+      install_requires=["django>=1.8, <3.2", "six"],
       tests_require=[
           'flake8',
       ],
