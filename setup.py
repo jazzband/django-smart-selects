a157b5e7625df951d3525529d91b475295f27cdb
Manel Clos <manelclos@gmail.com>
2021-04-10 09:08:27 +0200

    support Django 3.2

--- setup.py
+++ setup.py
@@ -24,5 +24,10 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
     ],
 )
